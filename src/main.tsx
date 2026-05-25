4eca33c642d0798093f41c827fce7c41bcae198f
jawadnajih27-spec <jawadnajih27@gmail.com>
2026-05-25 21:40:28 +0100

    Rename App component import to app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import app from "./app";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <app />
   </StrictMode>
 );
